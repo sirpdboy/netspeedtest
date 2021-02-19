8af3eeba9e6477f02927c9551419a8c942729766
sirpdboy <sirpdboy@qq.com>
2021-02-19 22:11:01 +0800

    up netspeedtest1.8

--- luci-app-NetSpeedTest/luasrc/controller/NetSpeedTest.lua
+++ luci-app-NetSpeedTest/luasrc/controller/NetSpeedTest.lua
@@ -4,34 +4,35 @@ module("luci.controller.netspeedtest", package.seeall)
 function index()
 
 	page = entry({"admin", "network",  "netspeedtest"}, template("netspeedtest"), "netspeedtest", 90)
-	page.dependent = true
-	page = entry({"admin", "network", "diag_iperf"}, call("diag_iperf"), nil)
+	page = entry({"admin", "network", "diag_iperf0"}, post("diag_iperf0"), nil)
 	page.leaf = true
-	page = entry({"admin", "network",  "diag_iperf6"}, call("diag_iperf6"), nil)
-	page.leaf = true	
-	page = entry({"admin", "network", "diag_speedtest1"}, call("diag_speedtest1"), nil)
+	page = entry({"admin", "network",  "diag_iperf1"}, post("diag_iperf1"), nil)
 	page.leaf = true
-	page = entry({"admin", "network", "diag_speedtest2"}, call("diag_speedtest2"), nil)
+	page = entry({"admin", "network", "diag_speedtest0"}, post("diag_speedtest0"), nil)
 	page.leaf = true
-	page = entry({"admin", "network", "diag_speedtest3"}, call("diag_speedtest3"), nil)
+	page = entry({"admin", "network", "diag_speedtest1"}, post("diag_speedtest1"), nil)
 	page.leaf = true
-	page = entry({"admin", "network", "diag_speedtest4"}, call("diag_speedtest4"), nil)
+	page = entry({"admin", "network", "diag_speedtest2"}, post("diag_speedtest2"), nil)
 	page.leaf = true
-	page = entry({"admin", "network", "diag_speedtest5"}, call("diag_speedtest5"), nil)
+	page = entry({"admin", "network", "diag_speedtest3"}, post("diag_speedtest3"), nil)
 	page.leaf = true
-	page = entry({"admin", "network", "diag_speedtest6"}, call("diag_speedtest6"), nil)
+	page = entry({"admin", "network", "diag_speedtest4"}, post("diag_speedtest4"), nil)
 	page.leaf = true
-	page = entry({"admin", "network", "diag_speedtest7"}, call("diag_speedtest7"), nil)
+	page = entry({"admin", "network", "diag_speedtest5"}, post("diag_speedtest5"), nil)
 	page.leaf = true
-	page = entry({"admin", "network", "diag_speedtest8"}, call("diag_speedtest8"), nil)
+	page = entry({"admin", "network", "diag_speedtest6"}, post("diag_speedtest6"), nil)
+	page.leaf = true
+	page = entry({"admin", "network", "diag_speedtest7"}, post("diag_speedtest7"), nil)
+	page.leaf = true
+	page = entry({"admin", "network", "diag_speedtest8"}, post("diag_speedtest8"), nil)
 	page.leaf = true
 end
 
 function diag_cmd(cmd, addr)
-	if addr and addr:match("^[a-zA-Z0-9%-%.:_]+$") then
+	if addr and addr:match("^[0-9]+$") then
 		luci.http.prepare_content("text/plain")
 
-		local util = io.popen(cmd % luci.util.shellquote(addr))
+		local util = io.popen(cmd % addr)
 		if util then
 			while true do
 				local ln = util:read("*l")
@@ -51,7 +52,6 @@ end
 
 function testlan(cmd, addr)
 		luci.http.prepare_content("text/plain")
-
 		local util = io.popen(cmd)
 		if util then
 			while true do
@@ -64,17 +64,19 @@ function testlan(cmd, addr)
 			util:close()
 		end
 
-		return
-
 end
 
 
-function diag_iperf(addr)
-	testlan("iperf3 -s 2>&1", addr)
+function diag_iperf0(addr)
+	testlan("iperf3 -s ", addr)
 end
 
-function diag_iperf6(addr)
-	testlan("iperf3 -s -B 0.0.0.0 2>&1", addr)
+function diag_iperf1(addr)
+	testlan("iperf3 -s -B 0.0.0.0 ", addr)
+end
+
+function diag_speedtest0(addr)
+	diag_cmd("ping -c 5 -W 1 %s 2>&1", addr)
 end
 
 function diag_speedtest1(addr)
